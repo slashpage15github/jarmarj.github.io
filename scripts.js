1044e7c49a3532cd7cc0679c141456edd6104275
jarmarj <jarmarj_first@hotmail.com>
2020-09-10 09:01:04 -0500

    added date input

--- scripts.js
+++ scripts.js
@@ -3,15 +3,19 @@ function submitForm(){
     var nombre = document.getElementById('nombres').value;
     var email = document.getElementById('email').value;
     var contraseña = document.getElementById('password').value;
-    verify(nombre, email, contraseña)
+    var fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    verify(nombre, email, contraseña, fechaNacimiento)
 }
-function verify(nombre, email, contraseña){
+function verify(nombre, email, contraseña, fechaNacimiento){
     if(nombre == null)
         alert("Falta nombre");
     if(email == null)
         alert("Falta email");
     if(contraseña == null)
         alert("Falta contraseña");
+    if(fechaNacimiento == null)
+        alert("Falta la fecha de nacimiento");
     if(Boolean(nombre) && Boolean(email) && Boolean(contraseña))
-        alert("Nombre:\n" + nombre + "\nE-mail:\n" + email + "\nContraseña:\n" + contraseña);
+        alert("Nombre:\n" + nombre + "\nE-mail:\n" + email + "\nContraseña:\n"
+         + contraseña + "\nFecha de nacimiento:\n" + fechaNacimiento);
 }
